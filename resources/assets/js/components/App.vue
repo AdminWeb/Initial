aac22bcbaddb0a9aa75cf6c1aac663dd548bcc74
IgorDePaula <principe.borodin@gmail.com>
2018-02-14 22:18:50 -0200

    initial commit

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+        <div id="app" class="container-fluid">
+           <div class="row">
+               Initial
+           </div>
+           <div class="row">
+               <router-view/>
+           </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "app"
+    }
+</script>
